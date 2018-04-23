5fa4a5da2924e8296270014573b2cc5a957fc375
Tarun Arora <tarunarora0807@gmail.com>
2018-04-23 14:43:43 -0700

    ship class added

---
+++ Pirates/Ship.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ship here.
+ * 
+ * @author (Tarun Arora) 
+ * @version (a version number or a date)
+ */
+public class Ship extends Actor
+{
+    private int fuel;
+    private int score;
+    private int count;
+   
+    public Ship() {
+        fuel = 30;
+        score = 0;
+        count = 0;
+        
+    }
+    /**
+     * Act - do whatever the Boat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+      {
+        
+    }
+}
