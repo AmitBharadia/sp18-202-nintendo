3f3a6cbe5aec1138fc4f46d72f5e9826e541cb3c
Rohit Sharma <rohit.sharma02@sjsu.edu>
2018-04-24 21:48:34 -0700

    startegy patter skeleton added

---
+++ Pirates/ShipMovement.java
@@ -0,0 +1,14 @@
+/**
+ * Write a description of class iStrategy here.
+ * 
+ * @author (Rohit) 
+ * @version (a version number or a date)
+ */
+import greenfoot.*;
+
+public class ShipMovement extends Actor implements iStrategy
+{
+    public void move(Actor a){
+       
+    }
+}
---
+++ Pirates/iStrategy.java
@@ -0,0 +1,11 @@
+/**
+ * Write a description of class iStrategy here.
+ * 
+ * @author (Rohit) 
+ * @version (a version number or a date)
+ */
+import greenfoot.*;
+
+public interface iStrategy{
+    void move(Actor a);
+}
