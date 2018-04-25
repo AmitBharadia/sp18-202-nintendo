5963f2d1a01b3fc5702fcde8ce0116694ba7cec3
Tarun Arora <tarunarora0807@gmail.com>
2018-04-24 21:56:38 -0700

    ocean actors added

---
+++ Pirates/Fuel.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mushroom here.
+ * 
+ * @author (Tarun Arora)
+ */
+public class Fuel extends Actor
+{
+        
+}
---
+++ Pirates/Gems.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Gems here.
+ * 
+ * @author (Tarun Arora)
+ */
+public class Gems extends Actor
+{
+    public void act() 
+    {
+        
+    }    
+}
