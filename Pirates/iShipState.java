81a0483535d7684a05b4ca0fa19ac3343d3829d9
srivatsa <you@example.com>
2018-04-24 18:49:37 -0700

    State Interface

---
+++ Pirates/iShipState.java
@@ -0,0 +1,11 @@
+/**
+ * Write a description of class iShipState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface iShipState  
+{
+    public void setState();
+    public void display();
+}
