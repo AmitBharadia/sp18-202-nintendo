d04ab5de237bd552fa4cbdaecce3cec23b664a17
rachitchokshi <rachitchokshi@gmail.com>
2018-04-26 06:37:56 -0700

    Fixed bug in Ocean class and added randomness

--- Pirates/Ocean.java
+++ Pirates/Ocean.java
@@ -8,9 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Ocean extends World
 {
+    OceanTreasures gemsFactory;
+    
     public Ocean()
     {    
         super(800, 600, 1); 
+        gemsFactory = new GemsFactory();
         
+        addObject(new Ship(), 270, 210);
+        addObject(gemsFactory.getActor(), (int)(10*Math.random())*60, (int)(8*Math.random())*60);
     }
 }
