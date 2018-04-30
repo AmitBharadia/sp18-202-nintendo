d7c998e8fd7cc94db4d28a8654e8d289f8659b96
srivatsa <you@example.com>
2018-04-30 11:09:45 -0700

    Final Banner

---
+++ Pirates/FinalBanner.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FinalBanner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FinalBanner extends Actor //this actor(image) is for displaying whether the player has won or lost.
+{
+    public FinalBanner(String image){
+        setImage(image);
+    }
+    
+    public void act(){}    
+}
