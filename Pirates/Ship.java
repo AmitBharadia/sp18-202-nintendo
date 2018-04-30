2d5c2410440dd47c0cf8fde575ef7372c306b7b4
Tarun Arora <tarunarora0807@gmail.com>
2018-04-30 09:45:28 -0700

    Factories used for ship

--- Pirates/Ship.java
+++ Pirates/Ship.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Ship here.
+ * Write a description of class Boat here.
  * 
  * @author (Tarun Arora) 
  * @version (a version number or a date)
@@ -11,13 +11,29 @@ public class Ship extends Actor
     private int fuel;
     private int score;
     private int count;
+        
+    OceanTreasures food;
+    OceanTreasures fuelFactory;
+    OceanTreasures gemsFactory;
+    OceanTreasures icebergFactory;
    
     public Ship() {
         fuel = 30;
         score = 0;
         count = 0;
         
+        fuelFactory = new FuelFactory();
+        icebergFactory = new IcebergFactory();
+        shipContext = new Context(new ShipMovement());
+        
     }
+    
+    public Ship (int fuel, int score) {
+        this.fuel = fuel;
+        this.score = score;
+        count = 0;
+    }
+    
     /**
      * Act - do whatever the Boat wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -26,6 +42,7 @@ public class Ship extends Actor
       {
         
     }
+    
     public int getFuel() {
         return this.fuel;
     }
